635090467feab33f0575701981a51f51f57994f4
ameknite <code.affidavit841@passinbox.com>
2024-10-08 20:20:52 -0600

    fix pgp route

--- src/data/routes.tsx
+++ src/data/routes.tsx
@@ -3,7 +3,7 @@ const links = "/links";
 const donate = "/donate";
 const crypto = "/donate/cryptocurrencies";
 const kofi = "/donate/kofi";
-const pgp_key = ".well-kown/pgp.key.txt";
+const pgp_key = ".well-known/pgp-key.txt";
 const not_found = "*paramName";
 
 export { crypto, donate, home, kofi, links, not_found, pgp_key };

c36fe951f20ae40639fc0201b991de0d32aebdac
ameknite <code.affidavit841@passinbox.com>
2024-10-17 14:23:40 -0600

    change accent color - new green

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -1,5 +1,5 @@
 const text_color = "#4C4F69";
 const background_color = "#EFF1F5";
-const accent_color = "#7287FD";
+const accent_color = "#96c476";
 
 export { accent_color, background_color, text_color };

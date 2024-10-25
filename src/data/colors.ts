33bcdc6061b7e38a0328b4517456d46de0c815b2
ameknite <code.affidavit841@passinbox.com>
2024-10-25 12:15:00 -0600

    lavander

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -1,5 +1,5 @@
 const text_color = "#4C4F69";
 const background_color = "#EFF1F5";
-const accent_color = "#96c476";
+const accent_color = "#7287fd";
 
 export { accent_color, background_color, text_color };
